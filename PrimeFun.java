7d45f4d39480be7459a697fca23ef52feedd6f07
Ruthik4 <ruthikrakonde4@gmail.com>
2025-01-23 11:53:33 +0530

    first commit

---
+++ PrimeFun.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Primefun {
+	public static int fun(int num1) {
+		int count=0;
+		for(int i=1;i<=num1;i++) {
+			if(num1 % i ==0) {
+				count++;
+				
+			}
+			
+		}
+		return count;
+		
+	}
+
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter any number: ");
+		int a=sc.nextInt();
+		if (fun(a)==2) System.out.println("yes");
+		else System.out.println("no");
+		sc.close();
+	}
+}

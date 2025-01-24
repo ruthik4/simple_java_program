a5a6309a941ab183c4fbd2a9a47bd093300cfe1f
Ruthik4 <ruthikrakonde4@gmail.com>
2025-01-24 18:47:09 +0530

    updated_codes

--- Pattern.java
+++ Pattern.java
@@ -1,11 +1,12 @@
 
+
 public class Pattern {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for(int r=5;r>=1;r--) {
+		for(int r=1;r<=5;r++) {
 			for(int c=5;c>=r;c--) {
-				System.out.print(c+" ");
+				 System.out.print(c+" ");
 			}
 			System.out.println("\n");
 		}
---
+++ Pyramid.java
@@ -0,0 +1,18 @@
+
+public class Pyramid {
+
+	public static void main(String[] args) {
+		for(int r=1,sp=10;r<=5;r++) {
+			for(int k=1;k<sp;k++) {
+				System.out.print(" ");
+			}
+			sp=sp-1;
+			for(int c=1;c<=r;c++) {
+				System.out.print(r+ " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}

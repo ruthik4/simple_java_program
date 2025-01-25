83a1ea0dbf0c9f2a4815235f6be26fef26adee5f
Ruthik4 <ruthikrakonde4@gmail.com>
2025-01-25 09:25:04 +0530

    updated_and_added_codes

--- Pattern.java
+++ Pattern.java
@@ -21,6 +21,18 @@ public class Pattern {
 			System.out.println();
 		}
 		
+System.out.println("fshgfsajdgf");		
+		
+//printing hallow rectangle
+
+		for(int i=1;i<=3;i++) {
+			for(int j=1;j<=7;j++) {
+				if(i==1 || i==3 || j==1 || j==7) System.out.print("*");
+				else System.out.print(" ");
+				
+			}
+			System.out.println();
+		}
 		
 		
 
